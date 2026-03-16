dd547796cb8f3b263e44be1e734f6fcfaed005c1
Gustavo H. Apolinário <134400943+SNOWFORSE@users.noreply.github.com>
2026-03-15 22:56:52 -0300

    Add files via upload

--- lista1ex6.c
+++ lista1ex6.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    double tamanhoBits, velocidadeBps, tempoSegundos;
 
-    
+    printf(" Calculadora de Tempo de Download \n");
+
+    printf("Digite o tamanho do arquivo (em bits): ");
+    scanf("%lf", &tamanhoBits);
+
+    printf("Digite a velocidade da conexao (em bits por segundo): ");
+    scanf("%lf", &velocidadeBps);
+
+    tempoSegundos = tamanhoBits / velocidadeBps;
+
+    printf("\nO tempo necessario para o download e de %.2f segundos.\n", tempoSegundos);
+
+    return 0;
 }
