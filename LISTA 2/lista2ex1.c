bfc9bdb98e41301542818642a1e440aa53bff5ab
Gustavo H. Apolinário <134400943+SNOWFORSE@users.noreply.github.com>
2026-03-15 23:29:57 -0300

    Add program to display double of an integer

---
+++ LISTA 2/lista2ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//Programa para mostrar o dobro de um número 
+
+int main() {
+    int num;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &num);
+    printf("O dobro de %d e %d\n", num, num * 2);
+    return 0;
+}
