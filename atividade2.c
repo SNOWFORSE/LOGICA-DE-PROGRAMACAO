186a998dc3e243c8156a92e62479d70830ec9c18
Gustavo H. Apolinário <134400943+SNOWFORSE@users.noreply.github.com>
2026-05-12 21:59:57 -0300

    Create atividade2.c

---
+++ atividade2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+	
+	float m[3][3];
+	int i, j ;
+	
+	for(i = 0; i<3; i++){
+		for(j = 0; j<3; j++){
+			printf("Digite um valor para a Matriz[%d][%d]", i, j);
+			scanf("%d", &m[i][j]);
+			
+			if()
+	
+return 0;	
+}
+
